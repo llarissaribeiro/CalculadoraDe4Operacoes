640a7e289e011013f0036f967eb518205ad6a321
Larissa Ribeiro <larissap.ribeiro@hotmail.com>
2023-03-29 10:14:59 -0300

    criação do projeto

---
+++ index.js
@@ -0,0 +1,10 @@
+let x = prompt("Insira o primeiro número")
+let y = prompt("Insira o segundo número")
+
+alert(
+  "Resultado: " + "\n" +
+  "\nSoma: " + (x + y) +
+  "\nSubtração: " + (x - y) +
+  "\nMultiplicação: " + (x * y) +
+  "\nDivisão: " + (x / y)
+)
